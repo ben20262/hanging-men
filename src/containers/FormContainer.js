a694cd9e411f4b19e826adc5fd89bc821ab78cd5
ben20262 <ben20262@aim.com>
2020-08-01 10:17:33 -0400

    added commented future updates

--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -1,7 +1,27 @@
 import React, {Component} from 'react'
+import { addWords } from '../actions/index'
+// import { connect } from 'react-redux'
 
 class FormContainer extends Component {
 
+    render() {
+        <div className='FormContainer'>
+            FormContainer
+        </div>
+    }
+
 }
+
+// const mapStateToProps = state => {
+//     return {
+//         words: state.words
+//     }
+// }
+
+// const mapDispatchToProps = dispatch => {
+//     return {
+//         addWords: dispatch(addWords)
+//     }
+// }
 
 export default FormContainer
