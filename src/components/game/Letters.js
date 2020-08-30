c7bae82fcb01e907fe3a343d3f424212403ea63f
ben20262 <ben20262@aim.com>
2020-08-30 12:35:02 -0400

    added handleclick function

--- src/components/game/Letters.js
+++ src/components/game/Letters.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
  
 class Letters extends Component {
+    
+    handleClick = letter => {
+        console.log(letter)
+    }
+    
     render() { 
         return (
             <div>
