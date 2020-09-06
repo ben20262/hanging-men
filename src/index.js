68ca644d7b9f829d8909ed711eb64ac1b76e2865
ben20262 <ben20262@aim.com>
2020-09-06 13:52:24 -0400

    fixed provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, Provider } from 'redux'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
-import hangingReducer from '..reducers/index.js'
+import hangingReducer from './reducers/index.js'
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(hangingReducer)
