e00bf154fae2e9ce4f6c7d633754fa2026b6c16e
ben20262 <ben20262@aim.com>
2020-09-06 14:27:49 -0400

    added letterClick function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,18 @@
-const addWords = wordString => {
+export const addWords = wordString => {
     let words = wordString.split(', ')
     return dispatch => {
         dispatch({
             type: 'ADD_WORDS',
             words
+        })
+    }
+}
+
+export const letterClick = letter => {
+    return dispatch => {
+        dispatch({
+            type: 'LETTER_CLICK',
+            letter
         })
     }
 }
