77283bfc7538ef2c8fda2b900e3905a8f40615eb
ben20262 <ben20262@aim.com>
2020-09-13 22:24:04 -0400

    removed mapStateToDispatch

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -20,10 +20,4 @@ const mapStateToProps = state => {
     }
 }
 
-const mapDispatchToProps = dispatch => {
-    return {
-        
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(GameContainer)
+export default connect(mapStateToProps)(GameContainer)
