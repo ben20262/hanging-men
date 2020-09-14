ae8b87d33288237612fa7715f5f7e130b55cdbe0
ben20262 <ben20262@aim.com>
2020-09-13 22:19:01 -0400

    added intangible class

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -14,6 +14,7 @@ class Form extends Component {
 
     handleSubmit = event => {
         event.preventDefault()
+        event.target.className = 'intangible'
         this.props.addWords(this.state.words)
     }
 
