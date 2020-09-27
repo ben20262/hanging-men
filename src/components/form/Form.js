a53c2e74e5c99d87f870cf8f1ddfbcba629bdc6c
ben20262 <ben20262@aim.com>
2020-09-27 15:58:27 -0400

    added new inputs

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -3,26 +3,33 @@ import React, {Component} from 'react'
 class Form extends Component {
 
     state = {
-        words: []
+        words: [],
+        number: 26,
+        hints: []
     }
 
     handleChange = event => {
         this.setState({
-            words: event.target.value
+            ...this.state,
+            [event.target.name]: event.target.value
         })
     }
 
     handleSubmit = event => {
         event.preventDefault()
         event.target.className = 'intangible'
-        this.props.addWords(this.state.words)
+        this.props.addWords(this.state)
     }
 
     render() {
         return (
             <form onSubmit={this.handleSubmit} >
                 <p>Enter the words you would like to play with seperated with a comma.</p>
-                <textarea onChange={this.handleChange} />
+                <textarea name='words' onChange={this.handleChange} />
+                <p>Enter number of extra letters.</p>
+                <input name='number' onChange={this.handleChange}></input>
+                <p>Enter hints.</p>
+                <textarea name='hints' onChange={this.handleChange} />
                 <br />
                 <input type='submit' />
             </form>
