a99a272ce954a1d6ffbad7fe0d6f0361c355edc6
ben20262 <ben20262@aim.com>
2020-09-27 16:08:05 -0400

    added disabled letters prop

--- src/components/game/Letters.js
+++ src/components/game/Letters.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Letter from './Letter'
- 
-let startLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+// adds more disabled for some reason
 const Letters = props => {
+    let startLetters = letters
+    let letterNumber = props.number + props.word.length
+    let letterArray = props.word
+    for ( var i = props.word.length; i <= letterNumber; i ) {
+        let letterIndex = Math.floor(Math.random() * startLetters.length)
+        let newLetter = startLetters[letterIndex]
+        if (!letterArray.includes(newLetter)) {
+            letterArray.push(newLetter)
+            i++
+        }
+        startLetters.slice(letterIndex, letterIndex + 1)
+    }
     return (
         <div className='Letters'>
-            {startLetters.map(letter => <Letter key={letter} letter={letter} handleClick={props.handleClick} />)}
+            {letters.map(letter => <Letter key={letter} letter={letter} handleClick={props.handleClick} letterArray={letterArray} />)}
         </div>
     );
 }
- 
+
 export default Letters;
