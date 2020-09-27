936979175ab53beb418a5821033582d7cae77d9e
ben20262 <ben20262@aim.com>
2020-09-27 16:08:21 -0400

    added disabled letters

--- src/components/game/Letter.js
+++ src/components/game/Letter.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
 const Letter = props => {
+
+    let letterBool = (props.letterArray.includes(props.letter)) ? false : true
+
+    let letterButton
+    
+    console.log(props)
+
+    if (letterBool) {
+        letterButton = <button onClick={event => props.handleClick(event, props.letter)} disabled={true} >
+                {props.letter}
+        </button> 
+    } else {
+        letterButton = <button onClick={event => props.handleClick(event, props.letter)} >
+                {props.letter}
+        </button> 
+    }
+
     return (
-        <button onClick={event => props.handleClick(event, props.letter)}>
-            {props.letter}
-        </button>
+        letterButton
     )
 }
 
