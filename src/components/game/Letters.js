0329eb4ba0ef7553148ef263f51bad9a3f24f247
ben20262 <ben20262@aim.com>
2020-10-04 19:34:47 -0400

    removed letterArray

--- src/components/game/Letters.js
+++ src/components/game/Letters.js
@@ -4,21 +4,9 @@ import Letter from './Letter'
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 // adds more disabled for some reason
 const Letters = props => {
-    let startLetters = letters
-    let letterNumber = props.number + props.word.length
-    let letterArray = props.word
-    for ( var i = props.word.length; i <= letterNumber; i ) {
-        let letterIndex = Math.floor(Math.random() * startLetters.length)
-        let newLetter = startLetters[letterIndex]
-        if (!letterArray.includes(newLetter)) {
-            letterArray.push(newLetter)
-            i++
-        }
-        startLetters.slice(letterIndex, letterIndex + 1)
-    }
     return (
         <div className='Letters'>
-            {letters.map(letter => <Letter key={letter} letter={letter} handleClick={props.handleClick} letterArray={letterArray} />)}
+            {letters.map(letter => <Letter key={letter} letter={letter} handleClick={props.handleClick} letterArray={props.letterArray} />)}
         </div>
     );
 }
